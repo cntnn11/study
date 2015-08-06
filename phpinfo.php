d94af332640017093a33ed11732c97a17a23aac3
cntnn11 <cntnn11@live.cn>
2015-08-06 17:11:25 +0800

    添加一个swoole的聊天服务端程序

--- phpinfo.php
+++ phpinfo.php
@@ -1,30 +1,4 @@
 <?php
-echo '<pre>';
-var_dump($_ENV);
-var_dump($_SERVER);
-echo '</pre>';
-
-$i = 0;
-$i++;
-
-$j=$i;
-$j++;
-echo $j+$i;
-
-echo '<br/>';
-echo date('Y年m月d日');
-echo '<br/>';
-echo substr(md5('svnALYcntnn11'), 0, 12);
-echo '<br/>';
-echo substr(md5('alyIcpTnn11'), 0, 12);
-echo '<br/>';
-echo substr(md5('alycntnn11blog'), 0, 12);
-echo '<br/>';
-echo substr(md5('callmepwd'), 0, 12);
-echo '<br/>';
-echo substr(md5('isonlyisme'), 0, 8);
-echo '<br/>';
-
 if( $_GET['rule'] == 'tnn11' )
 {
 	phpinfo();
